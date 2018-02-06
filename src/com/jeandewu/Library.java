54cf0e620e9a120510149d6ed25651e97297aa42
jeandewu <jeandewu@gmail.com>
2018-02-06 19:53:58 +0100

    Initial commit ver 0.1

--- src/com/jeandewu/Library.java
+++ src/com/jeandewu/Library.java
@@ -1,5 +1,23 @@
 package com.jeandewu;
 
-public class Library {
+class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.1";
 
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseDate = 2010;
+        int pages = 296;
+        String publisher = "Greg";
+        String isbn = "9788373271890";
+
+        System.out.print(appName);
+        System.out.println("Książki dostępne w bibliotece:");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
 }
