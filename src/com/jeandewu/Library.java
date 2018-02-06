fd6d69ca20db2e7edd40c66f5885daf7faa61824
jeandewu <jeandewu@gmail.com>
2018-02-06 19:26:33 +0100

    Initial commit 0

---
+++ src/com/jeandewu/Library.java
@@ -0,0 +1,5 @@
+package com.jeandewu;
+
+public class Library {
+
+}
---
+++ src/com/jeandewu/Main.java
@@ -0,0 +1,8 @@
+package com.jeandewu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
