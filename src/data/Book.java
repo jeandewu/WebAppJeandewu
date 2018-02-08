a982927b31f7b0b9b024e722461125b44889e48d
jeandewu <jeandewu@gmail.com>
2018-02-08 22:23:23 +0100

    Ver 1.4 - dadane toStringi i equals

--- src/data/Book.java
+++ src/data/Book.java
@@ -1,5 +1,8 @@
 package data;
 
+
+import java.util.Objects;
+
 public class Book extends Publication {
     // Pola
     private String author;
@@ -32,16 +35,35 @@ public class Book extends Publication {
     }
 
     // Konstruktory
-    public Book(String title, String author, int year, int pages, String publisher, String isbn) {
+    public Book(String title, String author, int year, int pages, String publisher,
+                String isbn) {
         super(year, title, publisher);
         this.setAuthor(author);
         this.setPages(pages);
         this.setIsbn(isbn);
     }
 
-    public void printInfo() {
-        String info = getTitle() + "; " + getAuthor() + "; " + getYear() + "; " + getPages()
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return getTitle() + "; " + getAuthor() + "; " + getYear() + "; " + getPages()
                 + "; " + getPublisher() + "; " + getIsbn();
-        System.out.println(info);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return getPages() == book.getPages() &&
+                Objects.equals(getAuthor(), book.getAuthor()) &&
+                Objects.equals(getIsbn(), book.getIsbn());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), getAuthor(), getPages(), getIsbn());
     }
 }
--- src/data/Magazine.java
+++ src/data/Magazine.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.Objects;
+
 public class Magazine extends Publication {
     private int month;
     private int day;
@@ -8,18 +10,23 @@ public class Magazine extends Publication {
     public int getMonth() {
         return month;
     }
+
     public void setMonth(int month) {
         this.month = month;
     }
+
     public int getDay() {
         return day;
     }
+
     public void setDay(int day) {
         this.day = day;
     }
+
     public String getLanguage() {
         return language;
     }
+
     public void setLanguage(String language) {
         this.language = language;
     }
@@ -31,9 +38,26 @@ public class Magazine extends Publication {
         setDay(day);
     }
 
-    public void printInfo() {
-        String info = getTitle() + "; " + getPublisher() + "; " + getYear() + "-"
+    @Override
+    public String toString() {
+        return getTitle() + "; " + "; " + getPublisher() + "; " + getYear() + "-"
                 + getMonth() + "-" + getDay() + "; " + getLanguage();
-        System.out.println(info);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Magazine)) return false;
+        if (!super.equals(o)) return false;
+        Magazine magazine = (Magazine) o;
+        return getMonth() == magazine.getMonth() &&
+                getDay() == magazine.getDay() &&
+                Objects.equals(getLanguage(), magazine.getLanguage());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), getMonth(), getDay(), getLanguage());
     }
 }
